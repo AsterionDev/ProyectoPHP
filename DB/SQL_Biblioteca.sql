9299e5375435becd188f144c851bf6c85e03f587
Brayan Andres Pinzon Benavides <andrespinzon1414@gmail.com>
2022-06-29 18:31:47 -0500

    database ok

--- DB/SQL_Biblioteca.sql
+++ DB/SQL_Biblioteca.sql
@@ -1,19 +1,19 @@
 CREATE TABLE Users (
-    Id_U INT NOT NULL,
+    Id_U INT NOT NULL AUTO_INCREMENT,
     Name VARCHAR(70) NOT NULL,
     Email VARCHAR(50) NOT NULL,
     PRIMARY KEY(Id_U)
 );
 
 CREATE TABLE Authors (
-    Id_A INT NOT NULL,
+    Id_A INT NOT NULL AUTO_INCREMENT,
     Name VARCHAR(70) NOT NULL,
     Country VARCHAR(100) NOT NULL,
     PRIMARY KEY(Id_A)
 );
 
 CREATE TABLE Books (
-    Id_B INT NOT NULL,
+    Id_B INT NOT NULL AUTO_INCREMENT,
     Title VARCHAR(50) NOT NULL,
     PRIMARY KEY(Id_B)
 );
@@ -34,83 +34,83 @@ CREATE TABLE UserBooks (
 
 
 INSERT INTO Users
-    (Id_U, Name, Email)
+    (Name, Email)
 VALUES
-    (00,'Andrés Jimenez', 'ajimenez@gmail.com'),
-    (01,'Daniel Sotelo', 'dsotelo@gmail.com'),
-    (02,'Sofia Austen', 'sausten@gmail.com'),
-    (03,'David Benavides', 'dbenavides@gmail.com'),
-    (04,'Patricia Martinez', 'pmartinez@gmail.com'),
-    (05,'Lizet Mena', 'lmena@gmail.com'),
-    (06,'Eduardo Melo', 'emelo@gmail.com'),
-    (07,'Christian Solarte', 'csolarte@gmail.com'),
-    (08,'Laura Gomez', 'lgomez@gmail.com'),
-    (09,'Katherine España', 'kespaña@gmail.com'),
-    (10,'Camila Ortega', 'cortega@gmail.com'),
-    (11,'Johan Rosero', 'jrosero@gmail.com'),
-    (12,'Paola Cadena', 'pcadena@gmail.com'),
-    (13,'Daniela Delgado', 'ddelgado@gmail.com'),
-    (14,'Gabriel Figueroa', 'gfigueroa@gmail.com'),
-    (15,'Javier Bravo', 'jbravo@gmail.com'),
-    (16,'Alexander Burbano', 'aburbano@gmail.com'),
-    (17,'Elizabet Navia', 'enavia@gmail.com'),
-    (18,'Sebastian Castillo', 'scastillo@gmail.com'),
-    (19,'Diana Garcia', 'dgarcia@gmail.com'),
-    (20,'Luisa Lopez', 'llopez@gmail.com')
+    ('Andrés Jimenez', 'ajimenez@gmail.com'),
+    ('Daniel Sotelo', 'dsotelo@gmail.com'),
+    ('Sofia Austen', 'sausten@gmail.com'),
+    ('David Benavides', 'dbenavides@gmail.com'),
+    ('Patricia Martinez', 'pmartinez@gmail.com'),
+    ('Lizet Mena', 'lmena@gmail.com'),
+    ('Eduardo Melo', 'emelo@gmail.com'),
+    ('Christian Solarte', 'csolarte@gmail.com'),
+    ('Laura Gomez', 'lgomez@gmail.com'),
+    ('Katherine España', 'kespaña@gmail.com'),
+    ('Camila Ortega', 'cortega@gmail.com'),
+    ('Johan Rosero', 'jrosero@gmail.com'),
+    ('Paola Cadena', 'pcadena@gmail.com'),
+    ('Daniela Delgado', 'ddelgado@gmail.com'),
+    ('Gabriel Figueroa', 'gfigueroa@gmail.com'),
+    ('Javier Bravo', 'jbravo@gmail.com'),
+    ('Alexander Burbano', 'aburbano@gmail.com'),
+    ('Elizabet Navia', 'enavia@gmail.com'),
+    ('Sebastian Castillo', 'scastillo@gmail.com'),
+    ('Diana Garcia', 'dgarcia@gmail.com'),
+    ('Luisa Lopez', 'llopez@gmail.com')
 ;
 
 
 INSERT INTO Authors
-    (Id_A, Name, Country)
+    (Name, Country)
 VALUES
-    (1,'J.D. Salinger', 'USA'),
-    (2,'F. Scott. Fitzgerald', 'USA'),
-    (3,'Jane Austen', 'UK'),
-    (4,'Scott Hanselman', 'USA'),
-    (5,'Jason N. Gaylord', 'USA'),
-    (6,'Pranav Rastogi', 'India'),
-    (7,'Todd Miranda', 'USA'),
-    (8,'Christian Wenz', 'USA'),
-    (9,'Jorge Luis Borges', 'Argentina'),
-    (10,'Julio Cortázar', 'Argentina'),
-    (11,'Howard P. Lovecraft', 'USA'),
-    (12,'Haruki Murakami', 'Japón'),
-    (13,'Isaac Asimov', 'Rusia'),
-    (14,'Mario Benedetti', 'Argentina'),
-    (15,'Fiodor Dostoievski', 'Rusia'),
-    (16,'Pablo Neruda', 'Chile'),
-    (17,'Oscar Wilde', 'Irlanda'),
-    (18,'Edgar Allan Poe', 'USA'),
-    (19,'Charles Dickens', 'Inglaterra'),
-    (20,'Mark Twain', 'Usa'),
-    (21,'Bram Stoker', 'Irlanda')
+    ('J.D. Salinger', 'USA'),
+    ('F. Scott. Fitzgerald', 'USA'),
+    ('Jane Austen', 'UK'),
+    ('Scott Hanselman', 'USA'),
+    ('Jason N. Gaylord', 'USA'),
+    ('Pranav Rastogi', 'India'),
+    ('Todd Miranda', 'USA'),
+    ('Christian Wenz', 'USA'),
+    ('Jorge Luis Borges', 'Argentina'),
+    ('Julio Cortázar', 'Argentina'),
+    ('Howard P. Lovecraft', 'USA'),
+    ('Haruki Murakami', 'Japón'),
+    ('Isaac Asimov', 'Rusia'),
+    ('Mario Benedetti', 'Argentina'),
+    ('Fiodor Dostoievski', 'Rusia'),
+    ('Pablo Neruda', 'Chile'),
+    ('Oscar Wilde', 'Irlanda'),
+    ('Edgar Allan Poe', 'USA'),
+    ('Charles Dickens', 'Inglaterra'),
+    ('Mark Twain', 'Usa'),
+    ('Bram Stoker', 'Irlanda')
 ;
 
 INSERT INTO Books
-    (Id_B, Title)
+    (Title)
 VALUES
-    (1, 'The Catcher in the Rye'),
-    (2, 'Nine Stories'),
-    (3, 'Franny and Zooey'),
-    (4, 'The Great Gatsby'),
-    (5, 'Tender id the Night'),
-    (6, 'Pride and Prejudice'),
-    (7, 'Professional ASP.NET 4.5 in C# and VB'),
-    (8, 'El Aleph'),
-    (9, 'Ficciones'),
-    (10, 'Rayuela'),
-    (11, 'Historias de cronopios y de famas'),
-    (12, 'Los mitos de Cthulhu'),
-    (13, 'Tokio Blues'),
-    (14,"El fin de la eternidad"),
-    (15, "La tregua"),
-    (16, 'Noches blancas'),
-    (17, 'Veinte poemas de amor y una canción desesperada'),
-    (18, 'El retrato de Dorian Gray'),
-    (19, 'El cuervo'),
-    (20, 'Cuento de navidad'),
-    (21, 'Las aventuras de Tom Sawyer'),
-    (22, 'Dracula')
+    ( 'The Catcher in the Rye'),
+    ( 'Nine Stories'),
+    ( 'Franny and Zooey'),
+    ( 'The Great Gatsby'),
+    ( 'Tender id the Night'),
+    ( 'Pride and Prejudice'),
+    ( 'Professional ASP.NET 4.5 in C# and VB'),
+    ( 'El Aleph'),
+    ( 'Ficciones'),
+    ( 'Rayuela'),
+    ( 'Historias de cronopios y de famas'),
+    ( 'Los mitos de Cthulhu'),
+    ( 'Tokio Blues'),
+    ("El fin de la eternidad"),
+    ( "La tregua"),
+    ( 'Noches blancas'),
+    ( 'Veinte poemas de amor y una canción desesperada'),
+    ( 'El retrato de Dorian Gray'),
+    ( 'El cuervo'),
+    ( 'Cuento de navidad'),
+    ( 'Las aventuras de Tom Sawyer'),
+    ( 'Dracula')
 ;
 
 INSERT INTO BooksAuthors
