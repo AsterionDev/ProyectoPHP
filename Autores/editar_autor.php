ed82339e36dfd9c7c5aef7158a8da33ab49f8d0e
David Sotelo <juandavid.sotelo.m@gmail.com>
2022-07-03 15:44:37 -0500

    Editar autor

--- Autores/editar_autor.php
+++ Autores/editar_autor.php
@@ -3,30 +3,54 @@ include("../DB/conexion_db.php");
 //CAPTURA LOS VALORES QUE TIENE EL ARREGLO GET
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
-    $query = "SELECT * FROM authors WHERE id_tarea=$id";
+    $query = "SELECT * FROM authors WHERE id_a=$id";
     $resultado = mysqli_query($conn, $query);
     if (mysqli_num_rows($resultado) == 1) {
         $fila = mysqli_fetch_array($resultado);
-        $titulo = $fila['titulo'];
-        $descripcion = $fila['descripcion'];
+        $name = $fila['Name'];
+        $country = $fila['Country'];
     }
 }
 //CAPTURA LOS VALORES QUE TIENE EL ARREGLO POST
 if (isset($_POST['bt_actualizar'])) {
     $id = $_GET['id'];
-    $titulo = $_POST['titulo'];
-    $descripcion = $_POST['descripcion'];
-    $query = "UPDATE tarea SET titulo = '$titulo',descripcion = '$descripcion'
-WHERE id_tarea = $id";
+    $name = $_POST['name'];
+    $country = $_POST['country'];
+    $query = "UPDATE authors SET Name = '$name',Country = '$country'
+WHERE id_a = $id";
 
     mysqli_query($conn, $query);
-    header("Location: index.php");
+    header("Location: gestionAutores.php");
 }
 ?>
-<form action="editar_tarea.php?id=<?php echo $_GET['id'] ?>" method="POST">
-    <label for="ltitulo">Titulo:</label><br>
-    <input type="text" id="titulo" name="titulo" value="<?php echo $titulo ?>"><br>
-    <label for="ldescripcion">Descripcion:</label><br>
-    <textarea name="descripcion" cols="30" rows="2"><?php echo $descripcion ?></textarea><br>
-    <button name="bt_actualizar" type="submit">Actualizar Tarea</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Gestión Autores</title>
+</head>
+
+<body class="container">
+    <div class="d-flex justify-content-center flex-column m-5 p-5">
+        <div class="text-center">
+            <h1>Editar autor <?php echo $id ?></h1>
+        </div>
+
+
+        <form action="editar_autor.php?id=<?php echo $_GET['id'] ?>" method="POST">
+            <label for="lname">Nombre:</label><br>
+            <input type="text" id="name" name="name" value="<?php echo $name ?>" class="form-control"><br>
+            <label for="lcountry">Pais:</label><br>
+            <input type="text" id="country" name="country" value="<?php echo $country ?>" class="form-control"><br>
+            <button name="bt_actualizar" type="submit" class="btn btn-primary">Actualizar Autor</button>
+        </form>
+    </div>
+</body>
+
+</html>
